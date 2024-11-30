e01c4b4d989f4d7301b57fb296b0e08715941bd0
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2024-11-30 01:09:57 +0100

    Create services.nix

---
+++ probook0/services.nix
@@ -0,0 +1,12 @@
+{ pkgs, config, ... }:
+{
+  services = {
+    fprintd = {
+      enable = true;
+      tod = {
+        enable = true;
+        driver = with pkgs [ libfprint-2-tod1-vfs0090 libfprint-2-tod1-goodix ];
+      };
+    };
+  };
+}
