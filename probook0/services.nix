6ca350e491942715050d3b27d321c4ea84c18861
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2024-11-30 01:20:01 +0100

    Update services.nix

--- probook0/services.nix
+++ probook0/services.nix
@@ -5,9 +5,7 @@
       enable = true;
       tod = {
         enable = true;
-        driver = with pkgs; [ 
-          libfprint-2-tod1-vfs0090 
-          libfprint-2-tod1-goodix 
+        driver = pkgs.libfprint-2-tod1-goodix
         ];
       };
     };
