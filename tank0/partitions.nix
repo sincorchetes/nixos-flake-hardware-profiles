d59c1ce6295fe3457e69e7872b8237ae030ce296
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2024-12-20 19:24:45 +0000

    Update partitions.nix

--- tank0/partitions.nix
+++ tank0/partitions.nix
@@ -6,12 +6,13 @@
     ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/5f9735d5-3a5d-479b-bfd2-d5d82306f1b8";
-      fsType = "f2fs";
+    { device = "/dev/disk/by-uuid/1408f194-89ea-44fd-8eff-cac5fe6e2519";
+      fsType = "ext4";
+      options = [ "discard" "noatime" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/491C-906D";
+    { device = "/dev/disk/by-uuid/30B8-7234";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
