da32ac4d123ece0b9245c5c9169725b6e13fdcfe
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2025-04-25 11:09:58 +0100

    Update gnome.nix

--- systemSettings/desktopEnvironments/gnome.nix
+++ systemSettings/desktopEnvironments/gnome.nix
@@ -32,13 +32,5 @@
   ];
 
   security.pam.services.gdm.enableGnomeKeyring = true;
-  systemd.user.services.gnome-keyring-daemon = {
-    Unit.Description = "GNOME Keyring Daemon";
-    Service = {
-      ExecStart = "/run/wrappers/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh";
-      Restart = "on-failure";
-    };
-    Install.WantedBy = [ "default.target" ];
-  };
   
 }
