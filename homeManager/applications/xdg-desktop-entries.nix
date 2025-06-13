843435ff4de809c62875350c565bb63f761f274a
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2025-06-13 02:23:45 +0100

    Update xdg-desktop-entries.nix

--- homeManager/applications/xdg-desktop-entries.nix
+++ homeManager/applications/xdg-desktop-entries.nix
@@ -1,13 +1,13 @@
 {
   xdg.desktopEntries = {
-    firefox-custom = {
+    pritunl-client = {
       name = "Pritunl Client";
       genericName = "VPN Client";
       comment = "Launch Pritunl Client";
       exec = "pritunl-client-electron";
       icon = "pritunl_client_electron";
       type = "Application";
-      categories = [ "Network" "TrayIcon" ];
+      categories = [ "Network" "Utility" ];
       terminal = false;
     };
   };
