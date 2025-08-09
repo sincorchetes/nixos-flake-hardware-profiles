e7669d5fcdeff544cd49736831e9ef628e6de602
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2025-08-09 11:12:01 +0100

    Update zsh.nix

--- homeManager/shellTools/zsh.nix
+++ homeManager/shellTools/zsh.nix
@@ -43,9 +43,7 @@
         open = "xdg-open";
         gg = "gcloud";
         #code = "code --enable-features=UseOzonePlatform --ozone-platform=wayland --disable-gpu";
-        nxupdate = ''
-        run0 bash -c "git -C /etc/nixos pull && run0 nixos-rebuild switch --flake /etc/nixos/#$(hostname)"
-        '';
+        nxupdate = "run0 nixos-rebuild switch --flake github:sincorchetes/nixos-flake-hardware-profiles#tank0";
         nxcboot = "run0 nixos-rebuild boot --flake /etc/nixos/#$(hostname)";
         nxcsys = "nix-collect-garbage -d && run0 nix-collect-garbage -d";
         sudo = "run0";
