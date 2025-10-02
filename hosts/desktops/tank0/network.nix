9eba49d80f0947ff728e765b8cf6b79b12f8870a
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2025-10-02 10:31:49 +0100

    Update network.nix

--- hosts/desktops/tank0/network.nix
+++ hosts/desktops/tank0/network.nix
@@ -1,3 +1,38 @@
 {
-    networking.hostName = "tank0";
+  networking = {
+    hostName = "tank0";
+    networkmanager = {
+      enable = true;
+      ensureProfiles = {
+        profiles = {
+          "kvm-bridge" = {
+            connection = {
+              id = "kvm-bridge-br0";
+              type = "bridge";
+              interface-name = "br0";
+            };
+            bridge = {
+              stp = false;
+            };
+            ipv4 = {
+              method = "auto";
+            };
+            ipv6 = {
+              method = "disabled";
+            };
+          };
+
+          "eno1-slave" = {
+            connection = {
+              id = "eno1-slave-to-br0";
+              type = "ethernet";
+              interface-name = "eno1";
+              master = "kvm-bridge-br0";
+              slave-type = "bridge";
+            };
+          };
+        };
+      };
+    };
+  };
 }
