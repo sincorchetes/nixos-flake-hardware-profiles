57eeba860e3b3e50f7daf2c20c228fc7e84b387f
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2025-10-02 11:17:46 +0100

    Update network.nix

--- hosts/desktops/tank0/network.nix
+++ hosts/desktops/tank0/network.nix
@@ -1,25 +1,3 @@
 {
-  networking.useNetworkd = true;
-
-  systemd.network.netdevs."10-br0" = {
-    netdevConfig = {
-      Name = "br0";
-      Kind = "bridge";
-    };
-  };
-
-  systemd.network.networks."20-br0-dhcp" = {
-    matchConfig = { Name = "br0"; };
-    networkConfig = {
-      DHCP = "ipv4";
-    };
-  };
-
-  systemd.network.networks."30-eno1-slave" = {
-    matchConfig = { Name = "eno1"; };
-    networkConfig = {
-      Bridge = "br0";
-    };
-  };
-
+    networking.hostName = "tank0";
 }
