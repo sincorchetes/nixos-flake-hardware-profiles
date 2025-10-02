ae0a0bdd2dd976628a35a14c5e2628cfed34f2ba
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2025-10-02 10:49:25 +0100

    Update network.nix

--- hosts/desktops/tank0/network.nix
+++ hosts/desktops/tank0/network.nix
@@ -1,38 +1,13 @@
 {
   networking = {
     hostName = "tank0";
+    bridges.br0.interfaces = [ "eno1" ];
     networkmanager = {
       enable = true;
-      ensureProfiles = {
-        profiles = {
-          "kvm-bridge" = {
-            connection = {
-              id = "kvm-bridge-br0";
-              type = "bridge";
-              interface-name = "br0";
-            };
-            bridge = {
-              stp = false;
-            };
-            ipv4 = {
-              method = "auto";
-            };
-            ipv6 = {
-              method = "disabled";
-            };
-          };
-
-          "eno1-slave" = {
-            connection = {
-              id = "eno1-slave-to-br0";
-              type = "ethernet";
-              interface-name = "eno1";
-              master = "kvm-bridge-br0";
-              slave-type = "bridge";
-            };
-          };
-        };
-      };
+      unmanaged = [
+        "interface-name:br0"
+        "interface-name:eno1"
+      ];
     };
   };
 }
