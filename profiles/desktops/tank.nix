b7b6298b267fe29f6c56eea73cb95a9bb499c1fa
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2025-10-06 19:54:47 +0100

    Update tank.nix

--- profiles/desktops/tank.nix
+++ profiles/desktops/tank.nix
@@ -48,6 +48,23 @@
   environment = {
     systemPackages = with pkgs; [
       microcodeAmd
+      glxinfo
+      vulkan-tools
+      (writeShellScriptBin "nvidia-offload" ''
+        export __NV_PRIME_RENDER_OFFLOAD=1
+        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export __VK_LAYER_NV_optimus=NVIDIA_only
+        exec "$@"
+      '')
+      
+      (writeShellScriptBin "prime-run" ''
+        export __NV_PRIME_RENDER_OFFLOAD=1
+        export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+        export __GLX_VENDOR_LIBRARY_NAME=nvidia
+        export __VK_LAYER_NV_optimus=NVIDIA_only
+        exec "$@"
+      '')
     ];
   };
 
