23c2ca1f89eb999f028b9e13b62def4ccd892846
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2025-10-06 13:19:09 +0100

    Update probook.nix

--- profiles/laptops/probook.nix
+++ profiles/laptops/probook.nix
@@ -68,13 +68,4 @@
       options = [ "noauto" ];
     };
   };
-
-  swapDevices = lib.optionals (builtins.pathExists "/swapfile") [
-    {
-      file = "/swapfile";
-      size = 4096;
-      options = [ "discard" ];
-    }
-  ];
-
 }
