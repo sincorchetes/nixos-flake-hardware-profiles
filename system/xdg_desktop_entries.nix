6b70fc0d1df0feaa4e991be36b3892066d577d46
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2025-10-06 20:24:59 +0100

    Update xdg_desktop_entries.nix

--- system/xdg_desktop_entries.nix
+++ system/xdg_desktop_entries.nix
@@ -10,5 +10,15 @@
       categories = [ "Network" "Utility" ];
       terminal = false;
     };
+    steam-nvidia = {
+      name = "Steam (NVIDIA)";
+      comment = "Steam launcher using NVIDIA GPU (prime-run)";
+      exec = "prime-run steam %U";
+      icon = "steam";
+      terminal = "false";
+      type= "Application";
+      categories = ["Game", "Entertainment"];
+      startupNotify = true;
+    };
   };
 }
