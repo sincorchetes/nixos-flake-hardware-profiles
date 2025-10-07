d0429a38c250a361f7d04a821c62128804febc40
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2025-10-08 00:12:24 +0100

    Create atlas.nix

---
+++ profiles/kvm/atlas.nix
@@ -0,0 +1,98 @@
+{ config, pkgs, lib, modulesPath, ... }:
+
+{
+  imports = [
+    ../../system/importer.nix
+    inputs.disko.nixosModules.disko
+  ];
+
+  hardware = {
+    cpu.amd.updateMicrocode = true;
+  };
+
+  services = {
+    spice-vdagentd.enable = true;
+    qemuGuest.enable = true;
+    xserver.videoDrivers = [ "modesetting" "fbdev" ];
+    zfs = {
+      autoScrub.enable = true;
+      trim.enable = true;
+    };
+  };
+
+  networking.hostName = "atlas0";
+
+  environment = {
+
+    systemPackages = with pkgs; [
+      microcodeAmd
+      glxinfo
+      spice-vdagent
+      spice-autorandr
+      vulkan-tools
+    ];
+  };
+
+  boot = {
+    kernelParams = [
+      "preempt=full"
+    ];
+    supportedFilesystems = [ "zfs" ];
+    initrd = {
+      supportedFilesystems = [ "zfs" ];
+      kernelModules = [ "zfs" ];
+    };
+  };
+
+  disko.devices = {
+    disk.main = {
+      type = "disk";
+      device = "/dev/vda";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "512MiB";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+            };
+          };
+          zfs = {
+            size = "100%";
+            content = {
+              type = "zfs";
+              pool = "zroot";
+            };
+          };
+        };
+      };
+    };
+
+    zpool.zroot = {
+      type = "zpool";
+      options = {
+        ashift = "12";
+        autotrim = "on";
+      };
+      rootFsOptions = {
+        compression = "zstd";
+        atime = "off";
+        xattr = "sa";
+        acltype = "posixacl";
+      };
+      datasets = {
+        "root" = {
+          type = "zfs_fs";
+          mountpoint = "/";
+          encryption = {
+            enable = true;
+            keyformat = "passphrase";
+          };
+        };
+      };
+    };
+  };
+}
