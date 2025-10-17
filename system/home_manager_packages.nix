91764a68d34190e83ac31d63184a353c53b40bef
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2025-10-17 14:20:51 +0100

    Update home_manager_packages.nix

--- system/home_manager_packages.nix
+++ system/home_manager_packages.nix
@@ -8,23 +8,24 @@ in
 {
   home.packages =
     (with pkgs; [
-      google-chrome firefox brave                                                                                 # Web Browsers
-      libreoffice typora                                                                                          # Office Tools
-      gimp inkscape obs-studio wl-color-picker                                                                    # Multimedia Tools
-      vlc mpv spotify stremio                                                                                     # Video Players
-      copyq _1password-gui easyeffects qpwgraph pavucontrol appimage-run endeavour heroic                         # Desktop Tools
-      unrar                                                                                                       # File Compression Software
-      flat-remix-gnome flat-remix-icon-theme                                                                      # Desktop Theme Software
-      rio alacritty kitty yazi imv asciinema tmux eza tree btop htop ncdu fastfetch bat zoxide                    # GUI Terminal Tools
-      waybar hyprshot hyprpaper wofi hyprpaper wl-clipboard mako desktop-file-utils blueman playerctl hypridle    # Hyprland Desktop
-      terraform terragrunt kubectl kubecolor k9s kubernetes-helm awscli2 gcloud minikube                          # DevOps Tools
-      vscode-fhs code-cursor helix vim jetbrains.pycharm-professional jetbrains.webstorm                          # IDE Software
-      jq pre-commit postman figma-linux stripe-cli                                                                # Development Tools
-      jetbrains.datagrip dbeaver-bin                                                                              # Database Management Tools
-      telegram-desktop slack discord                                                                              # Instant Messaging
-      transmission_4-gtk networkmanagerapplet                                                                     # Network Tools
-      nix-search-cli                                                                                              # Nixpkg Manager Tools
-      veracrypt                                                                                                   # Security Tools
+      google-chrome firefox brave                                                                             # Web Browsers
+      libreoffice typora                                                                                      # Office Tools
+      gimp inkscape obs-studio wl-color-picker                                                                # Multimedia Tools
+      vlc mpv spotify stremio                                                                                 # Video Players
+      copyq _1password-gui easyeffects qpwgraph pavucontrol appimage-run endeavour heroic                     # Desktop Tools
+      unrar                                                                                                   # File Compression Software
+      flat-remix-gnome flat-remix-icon-theme                                                                  # Desktop Theme Software
+      rio alacritty kitty yazi imv asciinema tmux eza tree btop htop ncdu fastfetch bat zoxide                # GUI Terminal Tools
+      waybar hyprshot hyprpaper wofi hyprpaper wl-clipboard mako desktop-file-utils                           # Hyprland Desktop
+      blueman playerctl hypridle hyprlock                                                                     # Hyprland Desktop
+      terraform terragrunt kubectl kubecolor k9s kubernetes-helm awscli2 gcloud minikube                      # DevOps Tools
+      vscode-fhs code-cursor helix vim jetbrains.pycharm-professional jetbrains.webstorm                      # IDE Software
+      jq pre-commit postman figma-linux stripe-cli                                                            # Development Tools
+      jetbrains.datagrip dbeaver-bin                                                                          # Database Management Tools
+      telegram-desktop slack discord                                                                          # Instant Messaging
+      transmission_4-gtk networkmanagerapplet                                                                 # Network Tools
+      nix-search-cli                                                                                          # Nixpkg Manager Tools
+      veracrypt                                                                                               # Security Tools
       
     ]);
     #++
