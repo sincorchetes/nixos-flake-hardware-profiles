b6c5aaa8f3048baaf9ae5d0b6ffb338b86d2ff69
Álvaro Castillo <sincorchetes@gmail.com>
2025-10-25 17:24:58 +0100

    Fix wofi settings

--- system/waybar/waybar.nix
+++ system/waybar/waybar.nix
@@ -115,5 +115,135 @@
                 };
             };
         };
+
+        style = ''
+                * {
+                    font-family: FontAwesome, Roboto, Helvetica, Arial, sans-serif;
+                    font-size: 16px;
+                }
+
+                window#waybar {
+                    background-color: #373740;
+                    color: white;
+                    transition-property: background-color;
+                    transition-duration: 0.5s;
+                }
+
+                window#waybar.hidden {
+                    opacity: 0.2;
+                }
+
+                window#waybar.termite {
+                    background-color: #3f3f3f;
+                }
+
+                window#waybar.chromium {
+                    background-color: #000000;
+                    border: none;
+                }
+
+                button {
+                    box-shadow: inset 0 -3px transparent;
+                    border: none;
+                    border-radius: 0px;
+                }
+
+                button:hover {
+                    background: inherit;
+                    box-shadow: inset 0 -3px #ffffff;
+                }
+
+                #workspaces button {
+                    padding: 0 5px;
+                    background-color: transparent;
+                    color: #ffffff;
+                }
+
+                #workspaces button:hover {
+                    background: rgba(0, 0, 0, 0.2);
+                }
+
+                #workspaces button.focused {
+                    background-color: #64727d;
+                    box-shadow: inset 0 -3px #ffffff;
+                }
+
+                #workspaces button.urgent {
+                    background-color: #eb4d4b;
+                }
+
+                #mode {
+                    background-color: #64727d;
+                    box-shadow: inset 0 -3px #ffffff;
+                }
+
+                #clock,
+                #cpu,
+                #memory,
+                #disk,
+                #power,
+                #temperature,
+                #wireplumber,
+                #tray,
+                #bluetooth,
+                #mode,
+                #custom-power,
+                #mpris,
+                #power-profiles-daemon {
+                    background-color: transparent;
+                    margin: 5px;
+                    padding-left: 10px;
+                    padding-right: 10px;
+                    border-color: #ecf0f1;
+                }
+
+                #clock:hover,
+                #cpu:hover,
+                #memory:hover,
+                #disk:hover,
+                #temperature:hover,
+                #wireplumber:hover,
+                #tray:hover,
+                #bluetooth:hover,
+                #mode:hover,
+                #mpris:hover,
+                #power-profiles-daemon:hover {
+                    background-color: rgba(43, 48, 59, 0.5);
+                    color: #ffffff;
+                }
+
+                .modules-left > widget:first-child > #workspaces {
+                    margin-left: 0;
+                }
+
+                .modules-right > widget:last-child > #workspaces {
+                    margin-right: 0;
+                }
+
+                label:focus {
+                    background-color: #000000;
+                }
+
+                #temperature.critical {
+                    background-color: #eb4d4b;
+                }    
+
+                #tray > .passive {
+                    -gtk-icon-effect: dim;
+                }
+
+                #tray > .needs-attention {
+                    -gtk-icon-effect: highlight;
+                    background-color: #eb4d4b;
+                }
+
+                #keyboard-state > label {
+                    padding: 0 5px;
+                }
+
+                #keyboard-state > label.locked {
+                    background: rgba(0, 0, 0, 0.2);
+                }
+        '';
     };
 }
