0473f5d6ec2651f3d1510b36fd073cd49a31a526
Álvaro Castillo <sincorchetes@gmail.com>
2025-10-25 18:40:08 +0100

    Replace fonts in home-manager

--- system/system_packages.nix
+++ system/system_packages.nix
@@ -2,23 +2,9 @@
 
 {
   environment.systemPackages = with pkgs; [
-    acpi lm_sensors pciutils inetutils lshw usbutils vdpauinfo kernelshark mesa binutils gnupg unzip       # System Tools
-    borgbackup openssl git zsh                                                                             # System Tools
-    tcpdump nmap p0f wireshark rustscan                                                                    # Network Security Tools
-    pritunl-client openfortivpn                                                                            # VPN Software
-    sops age cachix devenv                                                                                 # NixOS Management Software Tools
-    gnomeExtensions.pop-shell gnomeExtensions.easyeffects-preset-selector gnomeExtensions.appindicator     # GNOME Extensions
+    acpi lm_sensors pciutils inetutils lshw usbutils vdpauinfo kernelshark mesa binutils        # System Tools
+    pritunl-client openfortivpn                                                                 # VPN Software
+    cachix                                                                                      # NixOS Management Software Tools
   ];
 
-  fonts.packages = with pkgs; [
-    nerd-fonts._0xproto
-    nerd-fonts.droid-sans-mono
-    nerd-fonts.ubuntu-sans
-    nerd-fonts.ubuntu
-    nerd-fonts.jetbrains-mono
-    roboto
-    font-awesome
-    noto-fonts-emoji-blob-bin
-    powerline-fonts
-  ];
 }
