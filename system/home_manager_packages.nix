73e39f7d9f9c9dc1c9878e93cb08eee0305b4a4a
Álvaro Castillo <sincorchetes@gmail.com>
2025-10-25 17:51:52 +0100

    Add CopyQ services

--- system/home_manager_packages.nix
+++ system/home_manager_packages.nix
@@ -7,69 +7,66 @@ let
 in
 {
   programs = {
-    #rio.enable = true;
-    #tmux.enable = true;
-    #fastfetch.enable = true;
-    #mpv.enable = true;
-    #firefox.enable = true;
-    #obs-studio.enable = true;
-    bat.enable = true;
-    bottom.enable = true;
-    k9s.enable = true;
-    ripgrep.enable = true;
-    ripgrep-all.enable = true;
-    neovim.enable = true;
-    #asciinema.enable = true;    # Available in 25.11
-    #helix.enable = true;
-    git.enable = true;
-    #delta.enable = true;        # Available in 25.11
-    htop.enable = true;
-    imv.enable = true;
-    #hwatch.enable = true;       # Available in 25.11
-    hyprlock.enable = true;
-    #waybar.enable = true;
-    #hyprshot.enable = true;     # Available in 25.11
-    kubecolor.enable = true;
-    awscli.enable = true;
-    jq.enable= true;
-  };
+      #rio.enable = true;
+      #tmux.enable = true;
+      #fastfetch.enable = true;
+      #mpv.enable = true;
+      #firefox.enable = true;
+      #obs-studio.enable = true;
+      bat.enable = true;
+      bottom.enable = true;
+      k9s.enable = true;
+      ripgrep.enable = true;
+      ripgrep-all.enable = true;
+      neovim.enable = true;
+      #asciinema.enable = true;    # Available in 25.11
+      #helix.enable = true;
+      git.enable = true;
+      #delta.enable = true;        # Available in 25.11
+      htop.enable = true;
+      imv.enable = true;
+      #hwatch.enable = true;       # Available in 25.11
+      hyprlock.enable = true;
+      #waybar.enable = true;
+      #hyprshot.enable = true;     # Available in 25.11
+      kubecolor.enable = true;
+      awscli.enable = true;
+      jq.enable= true;
+    };
 
-  home = {
-    #services = {
-    #  mako = {
-    #    enable = true;
-    #  };
-    #  copyq.enable = true;
-    #  blueman-applet.enable = true;
-    #  network-manager-applet.enable = true;
-    #};
-    packages =
-      (with pkgs; [
-        rio tmux fastfetch mpv firefox obs-studio
-        google-chrome brave                                                                                     # Web Browsers
-        libreoffice typora                                                                                      # Office Tools
-        gimp inkscape wl-color-picker                                                                           # Multimedia Tools
-        vlc spotify stremio                                                                                     # Video Players
-        copyq _1password-gui easyeffects qpwgraph pavucontrol appimage-run endeavour heroic                     # Desktop Tools
-        unrar                                                                                                   # File Compression Software
-        flat-remix-gnome flat-remix-icon-theme                                                                  # Desktop Theme Software
-        eza tree ncdu delta hwatch                                                                              # Shell Tools
-        xh procs dust duf tldr sd glow hyperfine navi dogdns just chezmoi asciinema                             # Shell Tools
-        wl-clipboard mako desktop-file-utils hyprshot                                                 # Hyprland Desktop
-        blueman playerctl                                                                              # Hyprland Desktop
-        terraform terragrunt kubectl kubernetes-helm gcloud minikube                                            # DevOps Tools
-        vscode-fhs code-cursor jetbrains.pycharm-professional jetbrains.webstorm                                # IDE Software
-        pre-commit postman figma-linux stripe-cli                                                               # Development Tools
-        jetbrains.datagrip dbeaver-bin                                                                          # Database Management Tools
-        telegram-desktop slack discord                                                                          # Instant Messaging
-        transmission_4-gtk networkmanagerapplet                                                                 # Network Tools
-        nix-search-cli                                                                                          # Nixpkg Manager Tools
-        veracrypt                                                                                               # Security Tools
-        niri
-      ]);
-      #++
-      #(with pkgsUnstable; [
-      #  brave
-      #]);
-   };
+    services = {
+      copyq.enable = true;
+      blueman-applet.enable = true;
+      network-manager-applet.enable = true;
+      };
+    home = {
+
+      packages =
+        (with pkgs; [
+          rio tmux fastfetch mpv firefox obs-studio
+          google-chrome brave                                                                                     # Web Browsers
+          libreoffice typora                                                                                      # Office Tools
+          gimp inkscape wl-color-picker                                                                           # Multimedia Tools
+          vlc spotify stremio                                                                                     # Video Players
+          copyq _1password-gui easyeffects qpwgraph pavucontrol appimage-run endeavour heroic                     # Desktop Tools
+          unrar                                                                                                   # File Compression Software
+          flat-remix-gnome flat-remix-icon-theme                                                                  # Desktop Theme Software
+          eza tree ncdu delta hwatch                                                                              # Shell Tools
+          xh procs dust duf tldr sd glow hyperfine navi dogdns just chezmoi asciinema                             # Shell Tools
+          wl-clipboard desktop-file-utils hyprshot                                                 # Hyprland Desktop
+          playerctl                                                                              # Hyprland Desktop
+          terraform terragrunt kubectl kubernetes-helm gcloud minikube                                            # DevOps Tools
+          vscode-fhs code-cursor jetbrains.pycharm-professional jetbrains.webstorm                                # IDE Software
+          pre-commit postman figma-linux stripe-cli                                                               # Development Tools
+          jetbrains.datagrip dbeaver-bin                                                                          # Database Management Tools
+          telegram-desktop slack discord                                                                          # Instant Messaging
+          transmission_4-gtk                                                                 # Network Tools
+          nix-search-cli                                                                                          # Nixpkg Manager Tools
+          veracrypt                                                                                               # Security Tools
+        ]);
+        #++
+        #(with pkgsUnstable; [
+        #  brave
+        #]);
+    };
 }
