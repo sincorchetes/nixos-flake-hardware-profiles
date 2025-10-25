4848123780415b0f1b0eaa85e5ce07ebdbdc7eea
Álvaro Castillo <sincorchetes@gmail.com>
2025-10-25 23:56:32 +0100

    Fix mako home-manager

--- system/userland/mako/mako.nix
+++ system/userland/mako/mako.nix
@@ -1,11 +1,5 @@
 {
     services.mako = {
         enable = true;
-        settings = {
-            "input-field" = {
-                "monitor" = "";
-                "fade_on_empty" = false;
-            };
-        };
     };
 }
