5598c007141da49042f2094e4c734338eaeddfca
Álvaro Castillo <sincorchetes@gmail.com>
2025-10-26 00:15:21 +0100

    Add foot home-manager

--- system/userland/foot/foot.nix
+++ system/userland/foot/foot.nix
@@ -4,12 +4,15 @@
     enable = true;
     settings = {
       "main" = {
-        "font" = "FiraCode Nerd Font Mono:size=12";
+        "font" = "FiraCode Nerd Font Mono:size=11";
         "dpi-aware" = "yes";
         "term" = "foot";
       };
       "scrollback" = {
         "lines" = 30000;
+      };
+      "colors" = {
+        "alpha" = 0.8;
       };
     };
   };
