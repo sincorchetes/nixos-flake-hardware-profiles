f7537892f74eeb6f45b1c89231d0f7af040d08de
Álvaro Castillo <sincorchetes@gmail.com>
2025-10-25 17:39:33 +0100

    Add Hypridle

--- system/hypr/hypridle.nix
+++ system/hypr/hypridle.nix
@@ -8,33 +8,31 @@
                 after_sleep_cmd = "hyprctl dispatch dpms on";
             };
 
-            listener = {
-                timeout = 150;
-                on-timeout = "brightnessctl -s set 10";
-                on-resume = "brightnessctl -r";
-            };
-
-            listener = { 
-                timeout = 150;
-                on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
-                on-resume = "brightnessctl -rd rgb:kbd_backlight";
-            };
-
-            listener = {
-                timeout = 300;
-                on-timeout = "loginctl lock-session";
-            };
-
-            listener = {
-                timeout = 330;
-                on-timeout = "hyprctl dispatch dpms off";
-                on-resume = "hyprctl dispatch dpms on";
-            };
-
-            listener = { 
-                timeout = 1800;
-                on-timeout = "systemctl suspend";
-            };
+            listeners = [
+                {
+                    timeout = 150;
+                    on-timeout = "brightnessctl -s set 10";
+                    on-resume = "brightnessctl -r";
+                }
+                { 
+                    timeout = 150;
+                    on-timeout = "brightnessctl -sd rgb:kbd_backlight set 0";
+                    on-resume = "brightnessctl -rd rgb:kbd_backlight";
+                }
+                {
+                    timeout = 300;
+                    on-timeout = "loginctl lock-session";
+                }
+                {
+                    timeout = 330;
+                    on-timeout = "hyprctl dispatch dpms off";
+                    on-resume = "hyprctl dispatch dpms on";
+                }
+                { 
+                    timeout = 1800;
+                    on-timeout = "systemctl suspend";
+                }
+            ];
         };
     };
 }
