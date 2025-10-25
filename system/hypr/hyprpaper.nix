71c2998c6e7c4178535eb3a3e3bbcbccd6452582
Álvaro Castillo <sincorchetes@gmail.com>
2025-10-25 16:54:07 +0100

    Fix hyprpar config

--- system/hypr/hyprpaper.nix
+++ system/hypr/hyprpaper.nix
@@ -1,12 +1,14 @@
 {
     services.hyprpaper = {
         enable = true;
-        ipc = "on";
-        preload = [
-            "~/.config/hypr/wallpaper.png"
-        ];
-        wallpaper = [
-            ", ~/.config/hypr/wallpaper.png"
-        ];
+        settings = {
+            ipc = "on";
+            preload = [
+                "~/.config/hypr/wallpaper.png"
+            ];
+            wallpaper = [
+                ", ~/.config/hypr/wallpaper.png"
+            ];
+        };
     };
 }
