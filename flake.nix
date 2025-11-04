87fa984699e390a9275cc9a160ecf2d89f3bcbbc
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2025-11-04 08:08:02 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
 
   };
 
-  outputs = inputs@{ nixpkgs, sops-nix, home-manager, disko, ... }:
+  outputs = inputs@{ nixpkgs-unstable, sops-nix, home-manager, disko, ... }:
     let
       system = "x86_64-linux";
 
