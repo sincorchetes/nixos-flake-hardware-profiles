b949135fd3f420cc99565010d1deec4c52318db2
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2025-11-04 09:13:44 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
       stdenv.hostPlatform.system = "x86_64-linux";
       mkSystem = path:
         nixpkgs.lib.nixosSystem {
-          inherit system;
+          inherit stdenv.hostPlatform.system;
           modules = [
             path
             sops-nix.nixosModules.sops
