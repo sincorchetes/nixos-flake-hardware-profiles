7a2128df8530e50b71cb4d174c42808d4330f414
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2025-12-10 22:13:20 +0000

    Update tank.nix

--- profiles/desktops/tank.nix
+++ profiles/desktops/tank.nix
@@ -9,7 +9,15 @@
     cpu.amd.updateMicrocode = true;
     xpadneo.enable = true;
     nvidia = {
-      package = pkgs.linuxPackages_latest.nvidia_x11_beta;
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "590.44.01";
+        
+        sha256_64bit = "sha256-VbkVaKwElaazojfxkHnz/nN/5olk13ezkw/EQjhKPms=";
+        sha256_aarch64 = "sha256-gpqz07aFx+lBBOGPMCkbl5X8KBMPwDqsS+knPHpL/5g=";
+        openSha256 = "sha256-ft8FEnBotC9Bl+o4vQA1rWFuRe7gviD/j1B8t0MRL/o=";
+        settingsSha256 = "sha256-wVf1hku1l5OACiBeIePUMeZTWDQ4ueNvIk6BsW/RmF4=";
+        persistencedSha256 = "sha256-nHzD32EN77PG75hH9W8ArjKNY/7KY6kPKSAhxAWcuS4=";
+      };
       modesetting.enable = true;
       powerManagement.enable = true;
       powerManagement.finegrained = false;
