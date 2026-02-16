96b4da291defb6942a405f843a2ee6bfd4dbb729
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2026-02-16 17:08:48 +0000

    Create default.nix

---
+++ home/sincorchetes/default.nix
@@ -0,0 +1,57 @@
+{ pkgs, inputs, ... }:
+
+{
+  imports = [
+    ./shell/zsh.nix
+    ./shell/tmux.nix
+    ./editors/nvim.nix
+    ./desktop/hyprland.nix
+    ./desktop/foot.nix
+    ./desktop/entries.nix
+  ];
+
+  home = {
+    username = "sincorchetes";
+    homeDirectory = "/home/sincorchetes";
+    stateVersion = "25.11";
+
+    sessionVariables = {
+      EDITOR = "nvim";
+      XDG_CONFIG_HOME = "$HOME/.config";
+      SOPS_AGE_KEY_FILE = "$HOME/.config/sops/age/keys.txt";
+    };
+
+    packages = with pkgs; [
+      fastfetch mpv firefox obs-studio
+      google-chrome brave                                                                                     # Web Browsers
+      libreoffice typora anki-bin                                                                             # Office Tools
+      gimp inkscape wl-color-picker                                                                           # Multimedia Tools
+      vlc spotify spotify-tray                                                                                # Video/Audio Players
+      copyq _1password-gui easyeffects qpwgraph pavucontrol appimage-run endeavour                            # Desktop Tools
+      unrar unzip                                                                                             # File Compression Software
+      flat-remix-gnome flat-remix-icon-theme                                                                  # Desktop Theme Software
+      eza tree ncdu                                                                                           # Shell Tools
+      xh procs dust duf tldr sd glow hyperfine navi dogdns just chezmoi asciinema borgbackup                  # Shell Tools
+      wl-clipboard desktop-file-utils playerctl devenv                                                        # Hyprland Desktop
+      kubectl kubernetes-helm minikube gcloud                                                                 # DevOps Tools
+      vscode-fhs code-cursor jetbrains.pycharm-professional jetbrains.webstorm antigravity-fhs                # IDE Software
+      pre-commit postman figma-linux                                                                          # Development Tools
+      jetbrains.datagrip dbeaver-bin                                                                          # Database Management Tools
+      telegram-desktop slack discord                                                                          # Instant Messaging
+      transmission_4-gtk tcpdump nmap p0f wireshark rustscan openssl gnupg                                    # NetSec Tools
+      nix-search-cli                                                                                          # Nixpkg Manager Tools
+      nerd-fonts._0xproto nerd-fonts.droid-sans-mono nerd-fonts.ubuntu-sans                                   # Fonts
+      nerd-fonts.ubuntu nerd-fonts.jetbrains-mono nerd-fonts.fira-code roboto                                 # Fonts
+      font-awesome noto-fonts-emoji-blob-bin powerline-fonts                                                  # Fonts
+      gnomeExtensions.pop-shell gnomeExtensions.easyeffects-preset-selector gnomeExtensions.appindicator      # GNOME Extensions
+    ];
+  };
+
+  services = {
+    copyq.enable = true; 
+    blueman-applet.enable = true;
+    network-manager-applet.enable = true;
+  };
+
+  programs.home-manager.enable = true;
+}
