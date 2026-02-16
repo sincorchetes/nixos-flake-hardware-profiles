9ab48a6a66bfa1b05d9e7f0ac8f8facd1188d7b9
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2026-02-16 23:14:21 +0000

    Update users.nix

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -3,12 +3,12 @@
 {
   users.mutableUsers = true;
   users.users = {
-    root.hashedPasswordFile = config.sops.secrets.root_password.path;
+    #root.hashedPasswordFile = config.sops.secrets.root_password.path;
     sincorchetes = {
       isNormalUser = true;
       shell = pkgs.zsh;
       description = "Álvaro Castillo";
-      hashedPasswordFile = config.sops.secrets.sincorchetes_password.path;
+      #hashedPasswordFile = config.sops.secrets.sincorchetes_password.path;
       extraGroups = [ 
         "wheel" "networkmanager" "docker" "libvirtd" "video" "audio" "input" 
       ];
