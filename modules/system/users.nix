1435cffdd8926994195600ea2c943163eb39cbfd
Álvaro Castillo <sincorchetes@gmail.com>
2026-02-16 20:24:57 +0000

    Update flakes

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -1,14 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  sops = {
-    age.keyFile = "/home/sincorchetes/.config/sops/age/keys.txt";
-    defaultSopsFile = ../../secrets.yaml;
-    secrets = {
-      root_password.neededForUsers = true;
-      sincorchetes_password.neededForUsers = true;
-    };
-  };
 
   users.users = {
     root.hashedPasswordFile = config.sops.secrets.root_password.path;
