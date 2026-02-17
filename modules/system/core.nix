c2c149922edd6175039b58c6cfd4aa6b4245f86f
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2026-02-17 07:24:02 +0000

    Update core.nix

--- modules/system/core.nix
+++ modules/system/core.nix
@@ -1,11 +1,5 @@
 { config, pkgs, inputs, ... }: 
 
-let
-  gdk = pkgs.google-cloud-sdk.withExtraComponents( with pkgs.google-cloud-sdk.components; [
-    gke-gcloud-auth-plugin
-  ]);
-in
-
 {
   system.stateVersion = "25.11";
 
@@ -45,7 +39,6 @@ in
     uutils-coreutils-noprefix
     fwupd
     fwupd-efi
-    gdk
   ];
 
   programs = {
