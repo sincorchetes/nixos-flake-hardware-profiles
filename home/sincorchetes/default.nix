3c4127a73c86aa3cafeeb6225ce795b2bb399d81
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2026-02-17 15:32:43 +0000

    Update default.nix

--- home/sincorchetes/default.nix
+++ home/sincorchetes/default.nix
@@ -29,7 +29,7 @@ in
       gimp inkscape wl-color-picker vlc spotify spotify-tray copyq _1password-gui easyeffects kubecolor
       qpwgraph pavucontrol appimage-run endeavour unrar unzip flat-remix-gnome flat-remix-icon-theme                                                                 
       eza tree ncdu xh procs dust duf tldr sd glow hyperfine navi dogdns just chezmoi asciinema borgbackup                  
-      wl-clipboard desktop-file-utils playerctl devenv kubectl kubernetes-helm minikube
+      wl-clipboard desktop-file-utils playerctl devenv kubectl kubernetes-helm minikube direnv devenv
       vscode-fhs code-cursor jetbrains.pycharm jetbrains.webstorm antigravity-fhs pre-commit postman figma-linux 
       jetbrains.datagrip dbeaver-bin telegram-desktop slack discord transmission_4-gtk tcpdump nmap p0f wireshark 
       rustscan openssl gnupg nix-search-cli nerd-fonts._0xproto nerd-fonts.droid-sans-mono nerd-fonts.ubuntu-sans                                   
