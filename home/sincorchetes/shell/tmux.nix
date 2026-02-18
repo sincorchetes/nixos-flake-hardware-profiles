52efa1f6c8382641e8a1c212e7848cf6a792433e
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2026-02-18 13:54:09 +0000

    Update tmux.nix

--- home/sincorchetes/shell/tmux.nix
+++ home/sincorchetes/shell/tmux.nix
@@ -4,6 +4,7 @@
         mouse = true;
         extraConfig = ''
             bind g setw synchronize-panes \; display-message "Sync panes toggled"
+            bind-key -T copy-mode MouseDragEnd1Pane send-keys -X copy-pipe-and-cancel "wl-copy"
         '';
     };
 }
