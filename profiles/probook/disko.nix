454500ca929ee007d447353ac5dd55a7c7d6317e
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2026-02-18 15:12:19 +0000

    Update disko.nix

--- profiles/probook/disko.nix
+++ profiles/probook/disko.nix
@@ -1,80 +1,87 @@
 {
   disko.devices = {
-    zpool = {
-      rpool = {
-        type = "zpool";
-        mode = {
-          type = "single";
-          devices = [ "/dev/disk/by-partlabel/nixos-zfs" ];
+    disk.main = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {};
+      };
+    };
+
+    zpool.rpool = {
+      type = "zpool";
+      mode = {
+        type = "single";
+        devices = [ "/dev/disk/by-partlabel/nixos-zfs" ];
+      };
+      options = {
+        ashift = "12";
+        autotrim = "on";
+      };
+      rootFsOptions = {
+        encryption = "aes-256-gcm";
+        keyformat = "passphrase";
+        keylocation = "prompt";
+        compression = "lz4";
+        xattr = "sa";
+        atime = "off";
+        acltype = "posixacl";
+      };
+      datasets = {
+        "local/root" = {
+          type = "zfs_fs";
+          mountpoint = "/";
+          options.mountpoint = "legacy";
         };
-        options = {
-          ashift = "12";
-          autotrim = "on";
+        "local/nix" = {
+          type = "zfs_fs";
+          mountpoint = "/nix";
+          options = {
+            atime = "off";
+            mountpoint = "legacy";
+          };
         };
-        rootFsOptions = {
-          encryption = "aes-256-gcm";
-          keyformat = "passphrase";
-          keylocation = "prompt";
-          compression = "lz4";
-          xattr = "sa";
-          atime = "off";
-          acltype = "posixacl";
+        "local/vms" = {
+          type = "zfs_fs";
+          mountpoint = "/var/lib/libvirt/images";
+          options = {
+            mountpoint = "legacy";
+            recordsize = "64K";
+            "com.sun:auto-snapshot" = "false";
+          };
         };
-        datasets = {
-          "local/root" = {
-            type = "zfs_fs";
-            mountpoint = "/";
-            options.mountpoint = "legacy";
+        "local/docker" = {
+          type = "zfs_fs";
+          mountpoint = "/var/lib/docker";
+          options = {
+            mountpoint = "legacy";
+            recordsize = "128K";
           };
-          "local/nix" = {
-            type = "zfs_fs";
-            mountpoint = "/nix";
-            options = {
-              atime = "off";
-              mountpoint = "legacy";
-            };
+        };
+        "safe/home" = {
+          type = "zfs_fs";
+          mountpoint = "/home";
+          options.mountpoint = "legacy";
+        };
+        "safe/home/sincorchetes" = {
+          type = "zfs_fs";
+          mountpoint = "/home/sincorchetes";
+          options.mountpoint = "legacy";
+        };
+        "safe/home/sincorchetes/.cache" = {
+          type = "zfs_fs";
+          mountpoint = "/home/sincorchetes/.cache";
+          options = {
+            mountpoint = "legacy";
+            atime = "off";
+            "com.sun:auto-snapshot" = "false";
           };
-          "local/vms" = {
-            type = "zfs_fs";
-            mountpoint = "/var/lib/libvirt/images";
-            options = {
-              mountpoint = "legacy";
-              recordsize = "64K";
-              "com.sun:auto-snapshot" = "false";
-            };
-          };
-          "local/docker" = {
-            type = "zfs_fs";
-            mountpoint = "/var/lib/docker";
-            options = {
-              mountpoint = "legacy";
-              recordsize = "128K";
-            };
-          };
-          "safe/home" = {
-            type = "zfs_fs";
-            mountpoint = "/home";
-            options.mountpoint = "legacy";
-          };
-          "safe/home/sincorchetes" = {
-            type = "zfs_fs";
-            mountpoint = "/home/sincorchetes";
-            options.mountpoint = "legacy";
-          };
-          "safe/home/sincorchetes/.cache" = {
-            type = "zfs_fs";
-            mountpoint = "/home/sincorchetes/.cache";
-            options = {
-              mountpoint = "legacy";
-              atime = "off";
-              "com.sun:auto-snapshot" = "false";
-            };
-          };
-          "safe/home/sincorchetes/.local" = {
-            type = "zfs_fs";
-            mountpoint = "/home/sincorchetes/.local";
-            options.mountpoint = "legacy";
-          };
+        };
+        "safe/home/sincorchetes/.local" = {
+          type = "zfs_fs";
+          mountpoint = "/home/sincorchetes/.local";
+          options.mountpoint = "legacy";
         };
       };
     };
