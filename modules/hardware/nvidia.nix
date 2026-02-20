65dd144f2169af9ca399e70b8afb34980df24b87
Álvaro Castillo <sincorchetes@gmail.com>
2026-02-20 18:10:26 +0000

    Try new management power profile

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -3,21 +3,34 @@
 {
   services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware.graphics.enable32Bit = true;
-  hardware.nvidia = {
-    package = config.boot.kernelPackages.nvidia_x11;
-    modesetting.enable = true;
-    powerManagement.enable = true;
-    open = true;
-    nvidiaSettings = true;
+  hardware = {
+    graphics.enable32Bit = true;
+    nvidia-container-toolkit.enable = true;
+    nvidia = {
+      package = config.boot.kernelPackages.nvidia_x11;
+      modesetting.enable = true;
+      powerManagement.enable = true;
+      powerManagement.finegrained = false;
+      open = true;
+      nvidiaSettings = true;
+    };
   };
 
-  hardware.nvidia-container-toolkit.enable = true;
-
-  boot.kernelParams = [ "nvidia-drm.modeset=1" ];
+  boot.kernelParams = [
+    "nvidia-drm.modeset=1"
+    "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+    "nowatchdog"
+  ];
 
   environment.systemPackages = with pkgs; [
     vdpauinfo
     mesa
   ];
+
+  powerManagement.powerDownCommands = ''
+    ${pkgs.kmod}/bin/modprobe -r btusb
+  '';
+  powerManagement.resumeCommands = ''
+    ${pkgs.kmod}/bin/modprobe btusb
+  '';
 }
