adb27b3ae0fae5b5aeee2583f1a93f2f228f9341
Álvaro Castillo <31448043+sincorchetes@users.noreply.github.com>
2026-02-24 11:23:45 +0000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-gcloud-fix.url = "github:NixOS/nixpkgs/pull/492139/head";
+    nixpkgs-gemini-cli-fix.url = "github:NixOS/nixpkgs/pull/493629/head";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
