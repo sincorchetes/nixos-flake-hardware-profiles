f5993fd5a376c949bf170b17ce64cd498c4cff6f
Álvaro Castillo <sincorchetes@gmail.com>
2026-04-02 20:47:19 +0100

    EasyEffects

--- modules/hardware/bluetooth.nix
+++ modules/hardware/bluetooth.nix
@@ -22,6 +22,15 @@
       "bluez5.enable-sbc-xq" = true;
       "bluez5.enable-msbc" = true;
       "bluez5.enable-hw-volume" = true;
+      "bluez5.headset-roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      "bluez5.hfphsp-backend" = "native";
+      "bluez5.default.rate" = 48000;
+      "bluez5.default.channels" = 2;
+    };
+    "50-bluetooth-policy" = {
+      "wireplumber.settings" = {
+        "bluetooth.autoswitch-to-headset-profile" = false;
+      };
     };
   };
 }
